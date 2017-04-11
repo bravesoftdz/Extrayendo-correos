0fdf7f60aac50b5bd1628e1aa5f51e7181b9fcb5
apuromafo <astronado@gmail.com>
2017-04-11 17:48:53 -0300

    Update
    Update

--- Unit3.pas
+++ Unit3.pas
@@ -1,10 +1,11 @@
-{Apuromafo v1.1  07/04/2017}
+{ Apuromafo v1.1  07/04/2017 }
 unit Unit3;
 
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
@@ -24,58 +25,59 @@ var
 implementation
 
 {$R *.dfm}
+
 procedure GetFileNames(Path: string; TS: TStrings);
 var
   sr: TSearchRec;
 begin
-  if FindFirst(ExtractFilePath(Path)+'*'+ExtractFileExt(Path),32,sr) = 0 then
+  if FindFirst(ExtractFilePath(Path) + '*' + ExtractFileExt(Path), 32, sr) = 0
+  then
     repeat
       TS.Add(ExtractFilePath(Path) + sr.Name);
     until FindNext(sr) <> 0;
 end;
 
-
-
 procedure TForm3.Button1Click(Sender: TObject);
 var
-i: Int64;
-Tabla:TStrings;
-Tabla2:TStrings;
-MyString: String;
-Splitted: TArray<String>;
+  i: Int64;
+  Tabla: TStrings;
+  Tabla2: TStrings;
+  MyString: String;
+  Splitted: TArray<String>;
 begin
-//Tabla.Create;
-Tabla := TStringList.Create;
-Tabla.Clear;
-Tabla2 := TStringList.Create;
-Tabla2.Clear;
-with TFileOpenDialog.Create(nil) do
-  try
-// Options := [fdoPickFolders];
-    if Execute then
-     // ShowMessage(FileName);
-//    GetFileNames(FileName, Files);
-//  for i:= 0 to Files.Count-1 do
-Tabla.LoadFromFile(Filename);
-//Memo1.Lines.LoadFromFile(Filename)  ;
-//Memo2.Clear;
-//Memo2.Lines.BeginUpdate ;
-for i := 0 to Tabla.Count-1 do
-begin
-MyString :=  Tabla.Strings[i] ;
-//MyString :=  Tabla.Strings[i] ;
-Splitted := MyString.Split([':']);
-Tabla2.Add(Splitted[0]) ;
-//Memo2.Lines.Add(Splitted[0]);
-//Memo2.Lines.EndUpdate;
+  // Tabla.Create;
+  Tabla := TStringList.Create;
+  Tabla.Clear;
+  Tabla2 := TStringList.Create;
+  Tabla2.Clear;
+  with TFileOpenDialog.Create(nil) do
+    try
+      // Options := [fdoPickFolders];
+      if Execute then
+        // ShowMessage(FileName);
+        // GetFileNames(FileName, Files);
+        // for i:= 0 to Files.Count-1 do
+        Tabla.LoadFromFile(Filename);
+      // Memo1.Lines.LoadFromFile(Filename)  ;
+      // Memo2.Clear;
+      // Memo2.Lines.BeginUpdate ;
+      for i := 0 to Tabla.Count - 1 do
+      begin
+        MyString := Tabla.Strings[i];
+        // MyString :=  Tabla.Strings[i] ;
+        Splitted := MyString.Split([':']);
+        Tabla2.Add(Splitted[0]);
+        // Memo2.Lines.Add(Splitted[0]);
+        // Memo2.Lines.EndUpdate;
+      end;
+
+    finally
+      Free;
+      Tabla2.SaveToFile('Resultado.txt');
+      // Tabla.SaveToFile('Resultado.txt'+ FileName);
+      Tabla.Free;
+      Tabla2.Free;
+    end;
 end;
 
-  finally
-    Free;
-    Tabla2.SaveToFile('Resultado.txt');
-	// Tabla.SaveToFile('Resultado.txt'+ FileName);
-	Tabla.Free;
-	Tabla2.Free;
-  end;
-end;
 end.
